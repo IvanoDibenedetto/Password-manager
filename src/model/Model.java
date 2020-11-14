4e03a8f728897aea40f0007c9ed0a5d759e3211a
Ivano <i.dibenedetto9@studenti.uniba.it>
2020-11-14 01:34:30 +0100

    Service model

--- src/model/Model.java
+++ src/model/Model.java
@@ -1,54 +1,23 @@
 package model;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class Model {
 
-    private String _name;
-    private String _username;
-    private String _password;
-    private String _code;
+    private Map<String, String> dizionario;
 
+    public Model(){
+        dizionario = new HashMap<String, String>();
+    }
+    public void addField(String key, String value){
+        dizionario.put(key,value);
+    }
+    public String getField(String key){
+        return dizionario.get(key);
+    }
+    public void patchField(){
 
-    public Model(String _name, String _code){
-        this._name = _name;
-        this._code = _code;
     }
 
-    public Model(String _name, String _username, String _password){
-        this._name = _name;
-        this._username = _username;
-        this._password = _password;
-    }
-
-
-    public String get_name() {
-        return _name;
-    }
-
-    public void set_name(String _name) {
-        this._name = _name;
-    }
-
-    public String get_username() {
-        return _username;
-    }
-
-    public void set_username(String _username) {
-        this._username = _username;
-    }
-
-    public String get_password() {
-        return _password;
-    }
-
-    public void set_password(String _password) {
-        this._password = _password;
-    }
-
-    public String get_code() {
-        return _code;
-    }
-
-    public void set_code(String _code) {
-        this._code = _code;
-    }
 }
