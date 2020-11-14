3c9dfd0202c7c29969770a8dedbe1f8a6696d475
Ivano <i.dibenedetto9@studenti.uniba.it>
2020-11-14 01:36:00 +0100

    Service model

--- src/model/Model.java
+++ src/model/Model.java
@@ -5,16 +5,18 @@ import java.util.Map;
 
 public class Model {
 
-    private Map<String, String> dizionario;
+    private String _name;
+    private Map<String, String> _dizionario;
 
-    public Model(){
-        dizionario = new HashMap<String, String>();
+    public Model(String _name){
+        this._name = _name;
+        _dizionario = new HashMap<String, String>();
     }
     public void addField(String key, String value){
-        dizionario.put(key,value);
+        _dizionario.put(key,value);
     }
     public String getField(String key){
-        return dizionario.get(key);
+        return _dizionario.get(key);
     }
     public void patchField(){
 
