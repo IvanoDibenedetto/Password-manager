b6a96ef3907a7ba5434a9b610261a8e8d31cc657
Ivano <i.dibenedetto9@studenti.uniba.it>
2020-11-15 11:35:00 +0100

    Accessing fxml elements

--- src/controller/Controller.java
+++ src/controller/Controller.java
@@ -1,13 +1,32 @@
 package controller;
 
+import com.jfoenix.controls.JFXButton;
+import com.jfoenix.controls.JFXTextField;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
 
 import java.net.URL;
 import java.util.ResourceBundle;
 
 public class Controller implements Initializable {
+
+    @FXML
+    private JFXButton button1;
+
+    @FXML
+    private Label label1;
+
+    @FXML
+    private JFXTextField textview1;
+
+
+
     @Override
     public void initialize(URL location, ResourceBundle resources) {
-
+        label1.setText("initialized");
+        button1.setOnAction(event -> {label1.setText(textview1.getText());});
     }
+
+
 }
